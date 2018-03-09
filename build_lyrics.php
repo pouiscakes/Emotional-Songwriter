3bff9e737001805901b10e45ff77f2267a2adb34
pouiscakes <louisrlin@gmail.com>
2018-03-09 14:29:51 -0800

    basic build lyrics page with lyrics displayed

--- build_lyrics.php
+++ build_lyrics.php
@@ -1,33 +1,3 @@
-<?php
-// require 'dbserver_info.php';
-// require 'redirect.php';
-
-// session_save_path("/webpages/llin/coen174/sessions");
-// session_start(); // must start session before any HTML
-
-// error_reporting(0);
-// ini_set('display_errors', 0);
-
-// $conn = new mysqli($servername, $username, $password, $db_name);
-// if ($conn->connect_error) {
-//     die("Connection failed: " . $conn->connect_error);
-// }
-
-// $user_check = $_SESSION['login_user'];
-// $ses_sql = mysqli_query($conn,"select username from users where username = '$user_check' ");
-// $row = mysqli_fetch_array($ses_sql,MYSQLI_ASSOC);
-// $login_session = $row['username'];
-// if(!isset($_SESSION['login_user'])){ // should check $login_session actually
-//   redirect("login.php");
-// }
-?>
-
-<!-- <script>
-window.alert = function(msg) {
-    console.log(msg);
-}
-</script> -->
-
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -35,100 +5,38 @@ window.alert = function(msg) {
     <title>Build Lyrics</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="css/bootstrap.css" rel="stylesheet" media="screen">
-    <!-- <link href="css/style_courses.css" rel="stylesheet" media="screen"> -->
-    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script> -->
-    <!-- <script type="text/javascript" src="js/courses_filter_search.js"></script> -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+
+    <link href="css/mcc_style.css" rel="stylesheet" media="screen">
+    <link href='https://fonts.googleapis.com/css?family=Montserrat:100,200,300,400,500,600,700,800,900' rel='stylesheet'>
   </head>
   <body>
-  	<h1>Suggested Lyrics</h1>
+  	<h1 class="heading">Suggested Lyrics</h1>
     <div class="add_course_form" id="add_course_form" >
     <form action=<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?> method="post" >
 
   <?php
-    // echo exec('/DCNFS/users/web/pages/llin/seniordesign/seniordesign/bin/python markov.py', $output, $result);
-    exec('/Users/pouiscakes/.virtualenvs/seniordesign/bin/python markov.py', $output, $result);
-
-    for ($i = 0; $i < count($output); $i++) {
-      echo '<button type="submit" onclick="alert("Hello World!")">' . $output[$i] . '</button><br>';
+    $lyrics = file('lyrics/rhymed_lyrics_sadness.txt', FILE_IGNORE_NEW_LINES);
+    $lyrics_size = sizeof($lyrics);
+    // echo $lyrics_size;
+    for ($i = 0; $i < 3; $i++) {
+      $offset = 3 * mt_rand(0, $lyrics_size/3 - 1);
+      $line1 = $lyrics[$offset];
+      $line2 = $lyrics[$offset + 1];
+      echo '<button type="submit" class="lyricbox"><div>' . $line1 . '</div><div class="secondlyricline">' . $line2 . '</div></button><br>';
     }
   ?>
 
-      Write your own: <input type="text" name="notes">
-      <button type="submit" onclick="alert('Hello World!')">Use this Lyric</button><br>
-      <button type="submit" onclick="alert('Hello World!')">Regenerate Lyrics</button><br>
-      <button type="submit" onclick="alert('Hello World!')">Finish Song</button>
+      <!-- Write your own: <input type="text" name="notes"> -->
+      <!-- <button type="submit" onclick="alert('Hello World!')">Use this Lyric</button><br> -->
+      <button type="submit" class="button-control" onclick="alert('Hello World!')"><span class="glyphicon glyphicon-refresh"></span> &nbsp;&nbsp;Regenerate Lyrics</button><br>
+      <button type="submit" class="button-control" onclick="alert('Hello World!')"><span class="glyphicon glyphicon-ok"></span> &nbsp;&nbsp;Finish Song</button>
     </form>
   </div>
- <?php
-   
-
-
-  // $ses_sql = mysqli_query($conn,"select user_type from users where username = '$login_session' ");
-  // $row = mysqli_fetch_array($ses_sql,MYSQLI_ASSOC);
-  // $login_session = $row['user_type'];
-  // if($login_session == 'advisor') { 
-  //   require 'add_course_form.php';
-  // }
-  ?>
-
-<?php
-// // Create connection
-// $conn = new mysqli($servername, $username, $password, $db_name);
-
-// // Check connection
-// if ($conn->connect_error) {
-//     die("Connection failed: " . $conn->connect_error);
-// } 
-
-// if($login_session == 'advisor') {     
-//   // Check POST requests
-//   if( !empty($_POST['outside_school']) ) $outside_school = htmlspecialchars(mysqli_real_escape_string($conn, $_POST['outside_school']));
-//   if( !empty($_POST['outside_course']) ) $outside_course = htmlspecialchars(mysqli_real_escape_string($conn, $_POST['outside_course']));
-//   if( !empty($_POST['scu_course']) ) $scu_course = htmlspecialchars(mysqli_real_escape_string($conn, $_POST['scu_course']));
-//   if( !empty($_POST['equivalent']) ) $equivalent = htmlspecialchars(mysqli_real_escape_string($conn, $_POST['equivalent']));
-//   if( !empty($_POST['notes']) ) {
-//   	$notes = htmlspecialchars(mysqli_real_escape_string($conn, $_POST['notes']));
-//   } else{
-//   	$notes = "";
-//   }
-
-//   //sql to insert data
-//   if (!empty($outside_school) && !empty($outside_course) &&
-//    	!empty($scu_course) && !empty($equivalent)) {
-//   	$sql = "INSERT INTO courses (outside_school, outside_course, scu_course, equivalence, notes)
-//   	VALUES ('$outside_school', '$outside_course', '$scu_course', '$equivalent', '$notes')";
-
-//   	if ($conn->query($sql) === TRUE) {
-//   	    // echo "New record created successfully<br>";
-//   	} else {
-//   	    echo "<div class='error'>Course could not be added! A duplicate course entry already exists.</div><br>";
-//   	}
-//   } else {
-//   	if(!empty($equivalent))
-//   		echo "<div class='error'>Error: One or more required fields missing in form</div><br>";
-//   }
-// }
-?>
-
-<!-- 	<input type="text" id="course_search" name="course_search" onkeyup="filterSearch()" placeholder="🔍 Search" onfocus="this.placeholder = ''"
-onblur="this.placeholder = '🔍 Search'" />
-
-  <div id="live_data"></div>  -->
-
-<?php
-// mysqli_close($conn);
-?>
 
   </body>
 </html>
 
-<?php
-  // if($login_session == 'advisor') { 
-  //   require 'advisor_functions.php';
-  // }
-  // else {
-  //   require 'student_functions.php';
-  // }
 
-?>
 
