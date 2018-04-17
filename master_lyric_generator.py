e50bf6a8ccaa3e091f60c23f48199de83765fff9
pouiscakes <louisrlin@gmail.com>
2018-04-17 15:40:20 -0700

    cleaned joy for kids, reorganized previous lyrics

--- master_lyric_generator.py
+++ master_lyric_generator.py
@@ -1,12 +1,19 @@
+import merge_lyrics
+import emotion
 import markov
 import rhyme
 import vocabulary_count
 
-feeling = "joy"
-feelings = {"sadness", "anger", "fear", "surprise"}
+merge_lyrics.merge()
+emotion.sort()
+
+# feelings = {"joy", "sadness", "anger", "fear", "surprise"}
+feelings = {"joy"}
+
 for feeling in feelings:
 
     # generate in increments of 1000, then 100 until vocabulary stops increasing
+    # or just 3200
     markov.generate_lyric(feeling, 3200) 
     print("VOCABULARY COUNT: " + str(vocabulary_count.vocabulary_count("generated_lyrics_" + feeling + ".txt")))
 
